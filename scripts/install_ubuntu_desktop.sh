9e2ed4fe3feeb1b35379de2afd6e73cb92d52b79
Cai COooper <Cai.COooper@monitise.com>
2016-08-04 09:38:16 +0100

    removed unused scripts and baseline environment build

--- scripts/install_ubuntu_desktop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing ubuntu desktop"
-sudo apt-get -y -qq install --no-install-recommends ubuntu-desktop >/dev/null 2>/dev/null
-
-echo "Installing unity packages"
-sudo apt-get -y -qq install unity-lens-* unity-scope-home >/dev/null 2>/dev/null
